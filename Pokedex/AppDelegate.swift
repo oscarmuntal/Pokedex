15d92cdc8641256dd0735775be53f02984d09e55
Òscar Muntal <omuntal@ikomobi.com>
2017-11-02 05:05:03 +0100

    Added Realm configuration

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Defining the Schema version for Migration
+        let config = Realm.Configuration(
+            schemaVersion: 0,
+            migrationBlock: { migration, oldSchemaVersion in
+                switch oldSchemaVersion {
+                default: break
+                }
+        })
+        Realm.Configuration.defaultConfiguration = config
+        
         return true
     }
 
