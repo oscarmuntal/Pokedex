42c86864550965a3c7114f1132194680bcef83ec
Òscar Muntal <omuntal@ikomobi.com>
2017-11-02 06:10:21 +0100

    IBOutlets in BackPack screen and DataSource and Delegate for the CollectionView. Also added a Cell class for my CollectionView.

--- Pokedex/BackPackView.swift
+++ Pokedex/BackPackView.swift
@@ -15,6 +15,40 @@ protocol BackPackViewInterface {
 
 //MARK: BackPackView Class
 final class BackPackView: UserInterface {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var emptyListLabel: UILabel!
+    
+    @IBAction func searchButtonAction(_ sender: Any) {
+    
+    }
+    
+    let reuseIdentifier = "cell"
+}
+
+
+extension BackPackView: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        //TO DO
+    }
+    
+}
+
+extension BackPackView: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //TO DO
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        
+        return cell
+    }
+    
 }
 
 //MARK: - Public interface
---
+++ Pokedex/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  Pokedex
+//
+//  Created by Òscar Muntal on 02/11/2017.
+//  Copyright © 2017 Òscar Muntal. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
